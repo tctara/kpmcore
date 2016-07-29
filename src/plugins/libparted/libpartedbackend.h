b915f08862d19b54fe71c85184902a8ab542dd71
Andrius Štikonas <andrius@stikonas.eu>
2016-07-29 18:15:28 +0100

    Revert "Add KAuth helper to detectFileSystem."
    This reverts commit 71921cf500c5ae0cdde0176cf240e4a9fb282320.

--- src/plugins/libparted/libpartedbackend.h
+++ src/plugins/libparted/libpartedbackend.h
@@ -65,7 +65,7 @@ public:
     bool closeDevice(CoreBackendDevice* core_device) override;
     Device* scanDevice(const QString& deviceNode) override;
     QList<Device*> scanDevices(bool excludeReadOnly = true) override;
-    FileSystem::Type detectFileSystem(const QString& deviceNode) override;
+    FileSystem::Type detectFileSystem(const QString& partitionPath) override;
 
     static QString lastPartedExceptionMessage();
 
